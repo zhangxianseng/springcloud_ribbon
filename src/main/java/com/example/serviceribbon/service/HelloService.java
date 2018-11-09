ba2d250c849cecacd0426639a88c12360eb9a833
zzq <zha393150945@163.com>
2018-11-09 14:29:45 +0800

    Cloud服务消费者

---
+++ src/main/java/com/example/serviceribbon/service/HelloService.java
@@ -0,0 +1,23 @@
+package com.example.serviceribbon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Author: zzq
+ * @Description:
+ * @Date: 2018/11/6 15:29
+ */
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+
+
+}
